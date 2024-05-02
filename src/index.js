290c8ba9190e6bd6f20ff599d79ab6354ba82ed1
vladimirovicp <vladimirovicp70@gmail.com>
2024-05-02 14:34:01 +0400

    feat: основные настройки

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import "./scss/style.sass";
+import "./scss/_global.scss";
+import "./scss/_footer.scss";
---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = (env, argv) => {
+  const mode = argv.mode || 'development';
+  const isDevelopment = mode === 'development';
+  const devtool = isDevelopment ? 'source-map' : undefined;
+  return {
+    mode,
+    devtool,
+    entry: './src/index.js',
+    module: {
+      rules: [
+        {
+            test: /\.html$/,
+            loader: 'html-loader',
+        },
+        {
+            test: /\.js$/,
+            use: 'babel-loader',
+            exclude: /node_modules/,
+        },
+        {
+            test: /\.pug$/,
+            loader: 'pug-loader',
+            options: {
+                pretty: true,
+            },
+        },
+        {
+          test: /\.(jpg|png|svg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.scss$/,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.js'],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'src/front.pug'),
+        favicon: path.resolve(__dirname, 'src/assets/img/favicon.ico'),
+        filename: 'index.html',
+      }),
+    ],
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+      //assetModuleFilename: 'assets/[hash][ext]',
+      assetModuleFilename: 'assets/[name][ext]',
+    },
+    devServer: {
+      static: path.join(__dirname, 'src'),
+      compress: true,
+      port: 9000,
+      open: true,
+    },
+  };
+};
