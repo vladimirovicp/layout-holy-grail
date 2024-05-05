a3b216ae10b5945f1628a3b490f9bb0304230a16
vladimirovicp <vladimirovicp70@gmail.com>
2024-05-05 23:13:19 +0400

    feat: добавил коментарии

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -2,7 +2,6 @@
 const loginBox = document.querySelector('.login__box');
 
 if(loginBox){
-
     //меняем глах в поле пароль на странице логин
     const password = loginBox.querySelector('.field__password');
     const passwordInput = password.querySelector('input');
@@ -15,14 +14,15 @@ if(loginBox){
             passwordInput.type = 'password';
         }
     }
+    //end меняем глах в поле пароль на странице логин
 }
 
 
 
 const registerBox = document.querySelector('.register__box');
-
 if(registerBox){
 
+    // страница регистрации меняем инпут текст на календарь
     const birthDateBox = registerBox.querySelector('.field__birth-date');
     const birthDateInput = birthDateBox.querySelector('input');
 
@@ -35,8 +35,10 @@ if(registerBox){
         const el = event.target;
         el.type = 'text';
     });
+    //end страница регистрации меняем инпут текст на календарь
 
 
+    //меняем глах в поле пароль на странице регистрация
     const password = registerBox.querySelector('.field__password');
     const passwordInput = password.querySelector('input');
     const eye = password.querySelector('.eye');
@@ -48,17 +50,12 @@ if(registerBox){
             passwordInput.type = 'password';
         }
     }
+    //end меняем глах в поле пароль на странице регистрация
 
+
+    // Изменение цвета color на странице регистрации в селекте Сountry
     const country = registerBox.querySelector('.field__country');
     const countrySelect = country.querySelector('select');
-
-    // countrySelect.addEventListener('click', (event) => {
-    //     console.log(event.target.selectedIndex)
-    //     if( event.target.selectedIndex !== 0){
-    //         event.target.classList.add('active');
-    //     }
-    // });
-
     countrySelect.addEventListener('click',handleClickСountry);
     function handleClickСountry(event){
         if( event.target.selectedIndex !== 0){
@@ -66,6 +63,7 @@ if(registerBox){
             event.target.removeEventListener('click', handleClickСountry);
         }
     }
+    //End Изменение цвета
 
 
 
