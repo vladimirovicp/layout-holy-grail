a3933727fce00716b65aa17cd57772c21523b0a5
vladimirovicp <vladimirovicp70@gmail.com>
2024-05-06 14:47:23 +0400

    fix: правка путей

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import "./scss/style.sass";
-import "./scss/_fonts.scss";
+import "./assets/scss/style.sass";
+import "./assets/scss/_fonts.scss";
 
-import "./scss/_global.scss";
-import "./scss/_header.scss";
-import "./scss/_footer.scss";
+import "./assets/scss/_global.scss";
+import "./assets/scss/_header.scss";
+import "./assets/scss/_footer.scss";
 
-import "./scss/page/home.scss";
-import "./scss/page/login.scss";
-import "./scss/page/register.scss";
+import "./assets/scss/page/home.scss";
+import "./assets/scss/page/login.scss";
+import "./assets/scss/page/register.scss";
 
 
 import "./assets/js/script.js"
